6328b096cb40e3e8ccad12309e02493cdb8e1fba
francis <francist@hotmail.com>
2020-07-27 09:21:36 +0200

    ok

--- public/js/drag-drop-upload.js
+++ public/js/drag-drop-upload.js
@@ -27,7 +27,7 @@ var upcontrol = {
     // @TODO - ADD MORE POST DATA IF YOU WANT
     // data.append("foo", "bar");
     
-    xhr.open('POST', '/simple-upload.php', true);
+    xhr.open('POST', '../simple-upload.php', true);
     xhr.onload = function (e) {
       // SHOW UPLOAD STATUS
       var fstat = document.createElement('div'),
